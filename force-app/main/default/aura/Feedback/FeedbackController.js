9d8839a6854b9e9f9bd831ffc21989a38f18ec01
Evan <evan.devizio@gmail.com>
2021-08-17 17:49:46 -0400

    added Feedback custom object and Feedback aura component

---
+++ force-app/main/default/aura/Feedback/FeedbackController.js
@@ -0,0 +1,16 @@
+/**************************************************************
+* SupplierComponentController.js
+* Author: Evan DeVizio
+* Created: 8/16/21
+* Modified: 8/16/21
+***************************************************************/
+({
+    saveFeedback : function(component, event, helper) {
+        component.find("FeedbackForm").submit();
+        component.set("v.modalSwitch", true);
+    },
+ 
+   closeModel: function(component, event, helper) {
+      component.set("v.modalSwitch", false);
+   },
+})
--- force-app/main/default/aura/HarnessApp/HarnessApp.app
+++
@@ -1,3 +0,0 @@
-<aura:application extends="force:slds">
-    <c:tabsPage />
-</aura:application>
