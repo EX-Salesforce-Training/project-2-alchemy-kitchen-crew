1248ca3af19f63e6ed3a61a57e62cae80ff414f9
Jawad Chowdhury <jawadc13@gmail.com>
2021-08-20 11:56:11 -0500

    Try Buy Or Die button added and Gradient and death animation added

--- force-app/main/default/aura/TryBuyOrDie/TryBuyOrDieController.js
+++ force-app/main/default/aura/TryBuyOrDie/TryBuyOrDieController.js
@@ -3,15 +3,21 @@
 		
 	},
 
+        
+    
+    
+    
+    
     punchClick : function (cmp, event, helper) {
        // alert("You clicked: " + event.getSource().get("v.label"));
+       helper.checkHealth(cmp);
        console.log('just clicked punch');
        let zombieHealthHit = cmp.get('v.zombieHealth');
         console.log('got zombieHealth');
         let punchDamage = cmp.get('v.punchAttack')
         let afterZombieHit = cmp.get("c.Attacking");
         console.log('pointed to Attacking apex controller');
-        
+       helper.checkHealth(cmp);   
         afterZombieHit.setParams({
                 "zombieHealthHit" : zombieHealthHit,
             	"damage" : punchDamage,
@@ -24,6 +30,7 @@
                 if(response.getState() === "SUCCESS") {
                     console.log("I guess it worked?");
                     cmp.set("v.zombieHealth", response.getReturnValue());
+                    helper.checkHealth(cmp);
                     
                 }
                 else {
@@ -31,7 +38,7 @@
                     
                 }
             })
-        
+        helper.checkHealth(cmp);
         $A.enqueueAction(afterZombieHit);
 
     },
@@ -39,13 +46,14 @@
     
     knifeClick : function (cmp, event, helper) {
        // alert("You clicked: " + event.getSource().get("v.label"));
-       console.log('just clicked knife');
+       helper.checkHealth(cmp);
+        console.log('just clicked knife');
        let zombieHealthHit = cmp.get('v.zombieHealth');
         console.log('got zombieHealth');
         let knifeDamage = cmp.get('v.knifeAttack')
         let afterZombieHit = cmp.get("c.Attacking");
         console.log('pointed to Attacking apex controller');
-        
+        helper.checkHealth(cmp);
         afterZombieHit.setParams({
                 "zombieHealthHit" : zombieHealthHit,
             	"damage" : knifeDamage,
@@ -54,25 +62,29 @@
                 if(response.getState() === "SUCCESS") {
                     console.log("I guess it worked?");
                     cmp.set("v.zombieHealth", response.getReturnValue());
+                    helper.checkHealth(cmp);  
+                    
                 }
                 else {
                     console.log('Did not work :(');
                     
                 }
             })
+        helper.checkHealth(cmp);
         $A.enqueueAction(afterZombieHit);
     },
     
     
     batClick : function (cmp, event, helper) {
         // alert("You clicked: " + event.getSource().get("v.label"));
-       console.log('just clicked bat');
+       helper.checkHealth(cmp);
+        console.log('just clicked bat');
        let zombieHealthHit = cmp.get('v.zombieHealth');
         console.log('got zombieHealth');
         let batDamage = cmp.get('v.batAttack')
         let afterZombieHit = cmp.get("c.Attacking");
         console.log('pointed to Attacking apex controller');
-        
+        helper.checkHealth(cmp);
         afterZombieHit.setParams({
                 "zombieHealthHit" : zombieHealthHit,
             	"damage" : batDamage,
@@ -81,12 +93,14 @@
                 if(response.getState() === "SUCCESS") {
                     console.log("I guess it worked?");
                     cmp.set("v.zombieHealth", response.getReturnValue());
+					helper.checkHealth(cmp);                    
                 }
                 else {
                     console.log('Did not work :(');
                     
                 }
             })
+        helper.checkHealth(cmp);
         $A.enqueueAction(afterZombieHit);
     },
     
@@ -94,11 +108,13 @@
     
     katanaClick : function (cmp, event, helper) {
         // alert("You clicked: " + event.getSource().get("v.label"));
-       console.log('just clicked katana');
+       helper.checkHealth(cmp);
+        console.log('just clicked katana');
        let zombieHealthHit = cmp.get('v.zombieHealth');
         console.log('got zombieHealth');
         let katanaDamage = cmp.get('v.katanaAttack')
         let afterZombieHit = cmp.get("c.Attacking");
+        helper.checkHealth(cmp);  
         console.log('pointed to Attacking apex controller');
         
         afterZombieHit.setParams({
@@ -109,13 +125,35 @@
                 if(response.getState() === "SUCCESS") {
                     console.log("I guess it worked?");
                     cmp.set("v.zombieHealth", response.getReturnValue());
+                    helper.checkHealth(cmp);
+                    
                 }
                 else {
                     console.log('Did not work :(');
                     
                 }
             })
+        helper.checkHealth(cmp);
         $A.enqueueAction(afterZombieHit);
         
+    },
+    
+    
+     healthReset : function (cmp, event, helper) {
+        // alert("You clicked: " + event.getSource().get("v.label"));
+       
+         console.log('Health Reset Ran');
+       let zombieHealthHit = cmp.get('v.zombieHealth');
+         cmp.set("v.zombieHealth", 100);
+        console.log('added Health to zombie');
+         helper.checkHealth(cmp);
+         
+
+        
+
+       
+        
     }
+    
+    
 })
--- force-app/main/default/aura/TryBuyOrDie/TryBuyOrDieHelper.js
+++ force-app/main/default/aura/TryBuyOrDie/TryBuyOrDieHelper.js
@@ -1,5 +1,44 @@
 ({
-	helperMethod : function() {
-		
-	}
+checkHealth : function (cmp) {
+       
+       console.log('checking health...');
+       let zombieCurrentHealth = cmp.get('v.zombieHealth');
+        console.log('got zombieHealth');
+            
+            if (zombieCurrentHealth > 70) {
+              cmp.set("v.fullHealth", true);
+              cmp.set("v.midHealth", false);
+              cmp.set("v.lowHealth", false);
+
+               
+            } else {
+              if (zombieCurrentHealth > 30) {
+              cmp.set("v.fullHealth", false);
+              cmp.set("v.midHealth", true);
+              cmp.set("v.lowHealth", false);
+
+            } else {
+              if (zombieCurrentHealth > 0) {
+              cmp.set("v.fullHealth", false);
+              cmp.set("v.midHealth", false);
+              cmp.set("v.lowHealth", true);
+
+            } else {
+                
+              cmp.set("v.fullHealth", false);
+              cmp.set("v.midHealth", false);
+              cmp.set("v.lowHealth", false);
+                    
+                
+            }
+            }
+            }
+            
+            
+            
+            
+
+      
+
+    },
 })
